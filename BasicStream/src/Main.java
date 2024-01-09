f37ae3b03f6f6a172c35965411df9371a2e564d7
=VIVEK KUMAR <vivek.kumar08@nagarro.com>
2024-01-09 23:18:17 +0530

    Initial commit

--- BasicStream/src/Main.java
+++ BasicStream/src/Main.java
@@ -1,18 +1,25 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 //https://medium.com/dev-genius/java-8-coding-and-programming-interview-questions-and-answers-62512c44f062
+
+import java.util.List;
+import java.util.Arrays;
+
+//Given a list of integers, find out all the even numbers that exist in the list using Stream functions?
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        List<Integer> list = Arrays.asList(10,15,8,49,25,98,32);
+        list.stream()
+                .filter(n->n%2==0)
+                .forEach(x->System.out.print(" "+x));
+        System.out.println();
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        //Given a list of integers, find out all the numbers starting with 1 using Stream functions?
+        List<Integer> myList = Arrays.asList(10,15,8,49,25,98,32);
+            myList.stream()
+                    .map(s -> s + "")
+                    .filter(s -> s.startsWith("1"))
+                    .forEach(y->System.out.print(" "+y));
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
     }
 }
