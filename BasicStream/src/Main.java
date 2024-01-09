66ff220467e590670109b9f664cd73b4a647a4f5
=VIVEK KUMAR <vivek.kumar08@nagarro.com>
2024-01-09 23:50:08 +0530

    Initial commit

--- BasicStream/src/Main.java
+++ BasicStream/src/Main.java
@@ -2,8 +2,10 @@
 // then press Enter. You can now see whitespace characters in your code.
 //https://medium.com/dev-genius/java-8-coding-and-programming-interview-questions-and-answers-62512c44f062
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Arrays;
+import java.util.Set;
 
 //Given a list of integers, find out all the even numbers that exist in the list using Stream functions?
 public class Main {
@@ -13,6 +15,8 @@ public class Main {
                 .filter(n->n%2==0)
                 .forEach(x->System.out.print(" "+x));
         System.out.println();
+        //Output:
+        //10, 8, 98, 32
 
         //Given a list of integers, find out all the numbers starting with 1 using Stream functions?
         List<Integer> myList = Arrays.asList(10,15,8,49,25,98,32);
@@ -20,6 +24,30 @@ public class Main {
                     .map(s -> s + "")
                     .filter(s -> s.startsWith("1"))
                     .forEach(y->System.out.print(" "+y));
+        //Output:
+        //10, 15
+
+        System.out.println();
+        //How to find duplicate elements in a given integers list in java using Stream functions?
+        List<Integer> myList1 = Arrays.asList(10,15,8,49,25,98,98,32,15);
+        Set<Integer> set = new HashSet<>();
+
+
+        myList1.stream().filter(n-> !set.add(n)).forEach(y->System.out.print(" "+y));
+
+        System.out.println();
+
+        //Given the list of integers, find the first element of the list using Stream functions?
+
+        myList1.stream().limit(1).forEach(System.out::println);
+        //Output:
+        //10
+
+        //Given a list of integers, find the total number of elements present in the list using Stream functions?
+        System.out.println(myList1.stream().count());
+
+        //Given a list of integers, find the maximum value element present in it using Stream functions?
+        myList1.stream().max((x,y)->Integer.compare(x,y)).ifPresent(maxVal->System.out.println("Maximum value: "+ maxVal));
 
     }
 }
